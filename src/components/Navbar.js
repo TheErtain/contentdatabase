8db60f2b7b7b2286465b175619de13d2f8e0e2ef
William Watson <vivertain@gmail.com>
2019-01-02 15:03:33 -0800

    update 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,58 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-3">
         <div className="container">
           <Link to="/" className="navbar-brand">
-            <span className="text-success">
-              <i className="fas fa-database" />
-            </span>{" "}
-            Content Database
+            <i className="fas fa-database text-success" /> Content Database
           </Link>
+        </div>
+        <div className="dropdown mr-auto">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            data-toggle="dropdown"
+            type="button"
+          >
+            Select your content
+          </button>
+          <div className="dropdown-menu bg-secondary">
+            <ul type="none">
+              <li>
+                <i className="fas fa-film pr-2 text-white" />
+                <Link to="/bluray" className="text-warning pl-1">
+                  Blu-Ray
+                </Link>
+              </li>
+              <li>
+                <i className="fas fa-film pr-2 text-white" />
+                <Link to="/dvd" className="text-warning pl-1">
+                  DVD
+                </Link>
+              </li>
+              <li>
+                <i className="fas fa-film pr-2 text-white" />
+                <Link to="/dmovies" className="text-warning pl-1">
+                  Movies
+                </Link>
+              </li>
+              <li>
+                <i className="fas fa-tv pr-2 text-white" />
+                <Link to="/tv" className="text-warning">
+                  Tv Shows
+                </Link>
+              </li>
+              <li>
+                <i className="fab fa-artstation pr-2 text-white" />
+                <Link to="/anime" className="text-warning pl-1">
+                  Anime
+                </Link>
+              </li>
+              <div className="dropdown-divider" />
+              <li>
+                <i className="fab fa-artstation pr-2 text-white" />
+                <Link to="/" className="text-warning pl-1">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
--- src/layout/Anime.js
+++ src/layout/Anime.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
 
+const myBackgroundImage = require("./Animepic.jpg");
+const divStyle = {
+  width: "88%",
+  height: "800px",
+  backgroundImage: `url(${myBackgroundImage})`,
+  backgroundSize: "cover"
+};
+
 export default class Anime extends Component {
   render() {
     return (
-      <div>
-        <h1>Anime</h1>
+      <div style={divStyle}>
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="w-25 bg-secondary col-md-4">
+            <h1 className="text-white text-center">Anime</h1>
+            <input
+              type="text"
+              className="form-control mb-4"
+              name="animeName"
+              required
+              value=""
+              onChange=""
+              placeholder="Enter an anime name..."
+            />
+            <input
+              type="number"
+              className="form-control mb-4"
+              name="numberOfEps"
+              required
+              value=""
+              onChange=""
+              placeholder="How many episodes?"
+            />
+          </div>
+        </div>
       </div>
     );
   }
